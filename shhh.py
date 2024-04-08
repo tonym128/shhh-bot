0afbb4b1616a996109accd225ee334249e1729aa
Tony Mamacos <tmamacos@gmail.com>
2024-04-08 15:50:02 +0200

    Add ALLOWED_CHAT_IDS and make MY_CHAT_ID optional

--- shhh.py
+++ shhh.py
@@ -11,6 +11,7 @@ import re
 
 API_KEY = os.getenv('SHHH_API_KEY')
 MY_CHAT_ID = os.getenv('SHHH_MY_CHAT_ID')
+ALLOWED_CHAT_IDS = os.getenv('SHHH_ALLOWED_CHAT_IDS')
 
 logFormat = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
 logging.basicConfig(
@@ -30,25 +31,44 @@ def _esc_char(match):
 def my_escape(name):
     return _to_esc.sub(_esc_char, name)
 
+def checkUser(chat_id: str):
+    if ALLOWED_CHAT_IDS is None:
+        return True
+    allow_list = ALLOWED_CHAT_IDS.split(",")
+    if chat_id in allow_list:
+        return True
+
+    return False
+
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     txt = "Hi, I'm a bot who wants to help you keep quiet, let me take your voice notes and speech to text them!"
     await context.bot.send_message(chat_id=update.effective_chat.id, text=txt)
-    await context.bot.send_message(chat_id=MY_CHAT_ID, text=txt)
+    if MY_CHAT_ID is not None:
+        await context.bot.send_message(chat_id=MY_CHAT_ID, text=txt)
     logging.info("start - effective chat id: %s - txt: %s", update.effective_chat.id, txt)
 
 async def unknown(update: Update, context: ContextTypes.DEFAULT_TYPE):
     txt = "Sorry, I didn't understand that command."
     await context.bot.send_message(chat_id=update.effective_chat.id, text=txt)
-    await context.bot.send_message(chat_id=MY_CHAT_ID, text=txt)
+    if MY_CHAT_ID is not None:
+        await context.bot.send_message(chat_id=MY_CHAT_ID, text=txt)
     logging.info("unknown - effective chat id: %s - txt: %s", update.effective_chat.id, txt)
 
 async def handle_message(update, context):
     username = str(update.message.chat.username)
     chat_id = update.message.chat_id
+
+    if not checkUser(update.effective_chat.id):
+        logging.info("Not processing for " + username + " : " + update.effective_chat.id)
+        if MY_CHAT_ID is not None:
+            await context.bot.send_message(chat_id=MY_CHAT_ID, text="Started processing for "+username)
+        return
+
     start = time.time()
     fileid = uuid.uuid4().hex
     logging.info("Started processing for "+username)
-    await context.bot.send_message(chat_id=MY_CHAT_ID, text="Started processing for "+username)
+    if MY_CHAT_ID is not None:
+        await context.bot.send_message(chat_id=MY_CHAT_ID, text="Started processing for "+username)
     try:
         file = await context.bot.get_file(update.message.effective_attachment.file_id)
 
@@ -57,7 +77,8 @@ async def handle_message(update, context):
             end = time.time()
             logging.log(logging.INFO,str(end-start) + " " + username + " : " + str(chat_id) + ": FAIL SIZE : " + str(file.file_size) + "Message was too big for processing, there is a 50mb limit")
             await context.bot.send_message(chat_id=update.effective_chat.id, text="Message was too big for processing, there is a 50mb limit")
-            await context.bot.send_message(chat_id=MY_CHAT_ID, text="Message was too big for processing, there is a 50mb limit")
+            if MY_CHAT_ID is not None:
+                await context.bot.send_message(chat_id=MY_CHAT_ID, text="Message was too big for processing, there is a 50mb limit")
             return
 
         # Duration Check 650s
@@ -66,13 +87,15 @@ async def handle_message(update, context):
                 end = time.time()
                 logging.log(logging.INFO,str(end-start) + " " + username + " : " + str(chat_id)  + " : FAIL TIME : " + str(update.message.effective_attachment.duration) + " Cannot process audio longer than 60 seconds")
                 await context.bot.send_message(chat_id=update.effective_chat.id, text="Cannot process audio longer than 600 seconds")
-                await context.bot.send_message(chat_id=MY_CHAT_ID, text="Cannot process audio longer than 600 seconds")
+                if MY_CHAT_ID is not None:
+                    await context.bot.send_message(chat_id=MY_CHAT_ID, text="Cannot process audio longer than 600 seconds")
                 return
         except:
             end = time.time()
             logging.log(logging.INFO,str(end-start) + " " + username + " : " + str(chat_id)  + " : FAIL NOTIME : Does not look like a type I can process, exiting")
             await context.bot.send_message(chat_id=update.effective_chat.id, text="Does not look like a type I can process, exiting")
-            await context.bot.send_message(chat_id=MY_CHAT_ID, text="Does not look like a type I can process, exiting")
+            if MY_CHAT_ID is not None:
+                await context.bot.send_message(chat_id=MY_CHAT_ID, text="Does not look like a type I can process, exiting")
             return
 
         # Download and process
@@ -101,21 +124,20 @@ async def handle_message(update, context):
         db[str(username)] = str(value)
         db.close()
 
-        await context.bot.send_message(chat_id=MY_CHAT_ID, text=logline)
+        if MY_CHAT_ID is not None:
+            await context.bot.send_message(chat_id=MY_CHAT_ID, text=logline)
     except Exception as e :
         end = time.time()
         logging.log(logging.ERROR,str(end-start) + " " + username + " : " + str(chat_id)  + " : FAIL UNKNOWN : Failed processing message")
         logging.log(logging.ERROR,str(e))
         await context.bot.send_message(chat_id=update.effective_chat.id, text="Failure processing your message")
-        await context.bot.send_message(chat_id=MY_CHAT_ID, text="Failure processing your message")
+        if MY_CHAT_ID is not None:
+            await context.bot.send_message(chat_id=MY_CHAT_ID, text="Failure processing your message")
 
 if __name__ == '__main__':
     exitt = False
     if API_KEY == None:
         logging.info("SHHH_API_KEY must be defined")
-        exitt = True
-    if MY_CHAT_ID == None:
-        logging.info("SHHH_MY_CHAT_ID must be defined")
         exitt = True
 
     if not exitt:
