072b922fade5a5a65eb8a843ce19bd71f9392168
Tony Mamacos <tmamacos@gmail.com>
2024-04-11 11:45:23 +0200

    Whisper options

--- convert.sh
+++ convert.sh
@@ -1,5 +1,5 @@
 echo "Running FFMPEG"
 ffmpeg -y -i $1 -ar 16000 -ac 1 -c:a pcm_s16le $1.wav
 echo "Running Whisper"
-/usr/local/src/whisper SHHH_WHISPER_OPTIONS -otxt -m /usr/local/src/models/ggml-model.bin $1.wav
+/usr/local/src/whisper $SHHH_WHISPER_OPTIONS -otxt -m /usr/local/src/models/ggml-model.bin $1.wav
 echo "Complete"
