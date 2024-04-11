d65f7eaf7c162138ba63447206a5cd9d9286088b
Tony Mamacos <tmamacos@gmail.com>
2024-04-11 15:05:13 +0200

    Use /models for model store and symlink for model file for program

--- download.sh
+++ download.sh
@@ -1,16 +1,17 @@
 echo "Running Model Download"
-model=/usr/local/src/models/ggml-"$SHHH_WHISPER_MODEL".bin
+model=/models/ggml-"$SHHH_WHISPER_MODEL".bin
 echo $model
 
 if test -f "$model"; then
   echo "Model exists."
 else
   echo "Model does not exist. Downloading"
-  /usr/local/src/models/download-ggml-model.sh "$SHHH_WHISPER_MODEL"
+  cp ./models/download-ggml-model.sh /models/download-ggml-model.sh
+  /models/download-ggml-model.sh "$SHHH_WHISPER_MODEL"
 fi
 
 if ! cmp -s "$model" /usr/local/src/models/ggml-model.bin; then
-    echo "Copy model to replace existing one"
-    cp $model /usr/local/src/models/ggml-model.bin
+    echo "Link model to whisper one"
+    ln -s $model /usr/local/src/models/ggml-model.bin
     echo "Complete"
 fi
