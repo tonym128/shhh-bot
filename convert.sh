64ecc98d717d82a5a743643c77fa05d00325e068
Tony Mamacos <tmamacos@gmail.com>
2024-04-15 22:36:14 +0000

    update again

--- convert.sh
+++ convert.sh
@@ -1,5 +1,5 @@
 echo "Running FFMPEG"
-ffmpeg -y -i $0 -ar 16000 -ac 1 -c:a pcm_s16le $0.wav
+ffmpeg -y -i $1 -ar 16000 -ac 1 -c:a pcm_s16le $1.wav
 echo "Running Whisper"
-/usr/local/src/whisper $SHHH_WHISPER_OPTIONS -otxt -m /usr/local/src/models/ggml-model.bin $0.wav
+/usr/local/src/whisper $SHHH_WHISPER_OPTIONS -otxt -m /usr/local/src/models/ggml-model.bin $1.wav
 echo "Complete"
